2380b49199ba30526145979135d6a16bd178372d
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-02-13 12:03:23 -0600

    Update: Header & Footer components

--- Money-Coach/src/app/shared/shared.module.ts
+++ Money-Coach/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/header/button/button.component';
 
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule
