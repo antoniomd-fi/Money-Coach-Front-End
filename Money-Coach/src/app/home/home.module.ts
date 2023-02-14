b44ab84d6351bc0de683ac7e7b434c24cf5853d1
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-02-13 20:57:18 -0600

    Update: About component

--- Money-Coach/src/app/home/home.module.ts
+++ Money-Coach/src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/banner/about/about.component';
+import { CarouselComponent } from './components/banner/about/carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
