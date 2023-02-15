671b5c048326fb08746221a16e49af88d2a2b875
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-02-15 13:03:17 -0600

    Update: Entry & Exit Component

--- Money-Coach/src/app/user/components/user-panel/entries/entries.component.ts
+++ Money-Coach/src/app/user/components/user-panel/entries/entries.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EntriesService } from 'src/app/services/entry/entries.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-entries',
@@ -17,22 +18,40 @@ export class EntriesComponent {
   }
 
   entries : any
-  constructor (private entriesServices: EntriesService,private route: ActivatedRoute){}
+  constructor (private entriesServices: EntriesService,private route: ActivatedRoute, private httpClient : HttpClient){}
 
   existRegisters(){
     return this.entries.length > 0
   }
 
   delete(id:number){
-    for (let i = 0; i < this.entries.length; i++){
-      if (this.entries[i].id == id){
-        this.entries.splice(i, 1)
-        return
+    let decision = confirm("Are you sure to delete this register?. The action cannot be revert")
+    if (decision) {
+      for (let i = 0; i < this.entries.length; i++) {
+        if (this.entries[i].id == id) {
+          this.entries.splice(i, 1)
+          this.httpClient.delete(`${this.entriesServices.url}/deleteEntry/${id}`).subscribe(
+            res => {
+              console.log(res)
+            }, (error: any) => {
+              console.log(error);
+            }
+          )
+
+          return
+        }
       }
     }
+
+    alert("Operation canceled")
   }
 
   add (){
+
+    let id: any
+    id = this.route.snapshot.params
+
+
     if (this.entry.id == 0){
       alert("You must type a code diferent to 0")
       return
@@ -44,7 +63,16 @@ export class EntriesComponent {
       }
     }
 
-    this.entries.push({id:this.entry.id, amount: this.entry.amount, concept: this.entry.concept})
+    this.entries.push({id:this.entry.id, amount: this.entry.amount, concept: this.entry.concept, date: this.entry.date})
+
+    this.httpClient.post(this.entriesServices.url + '/addEntry', { id: this.entry.id, amount: this.entry.amount, concept: this.entry.concept, date: this.entry.date, personId: id.id })
+    .subscribe(
+      res => {
+        console.log(res)
+      }, (error: any) => {
+        console.log(error)
+      }
+    )
 
     this.entry.id = 0
     this.entry.amount = 0.0
@@ -53,10 +81,11 @@ export class EntriesComponent {
 
   }
 
-  select(entry: {id:number, amount:number, concept:string}){
+  select(entry: {id:number, amount:number, concept:string, date:string}){
     this.entry.id = entry.id
     this.entry.amount = entry.amount
     this.entry.concept = entry.concept
+    this.entry.date = entry.date
 }
 
 modify(){
--- Money-Coach/src/app/user/components/user-panel/exits/exits.component.ts
+++ Money-Coach/src/app/user/components/user-panel/exits/exits.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ExitsService } from 'src/app/services/exit/exits.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-exits',
@@ -8,42 +10,68 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./exits.component.css']
 })
 export class ExitsComponent {
-  exit={
-    id:0,
-    amount:0.0,
+  exit = {
+    id: 0,
+    amount: 0.0,
     concept: "",
     date: ""
   }
 
-  exits : any
-  constructor (private enxitsServices: ExitsService,private route: ActivatedRoute){}
+  exits: any
+  constructor(private enxitsServices: ExitsService, private route: ActivatedRoute, private httpClient: HttpClient) { }
 
-  existRegisters(){
+  existRegisters() {
     return this.exits.length > 0
   }
 
-  delete(id:number){
-    for (let i = 0; i < this.exits.length; i++){
-      if (this.exits[i].id == id){
-        this.exits.splice(i, 1)
-        return
+  delete(id: number) {
+    let decision = confirm("Are you sure to delete this register?. The action cannot be revert")
+    if (decision) {
+      for (let i = 0; i < this.exits.length; i++) {
+        if (this.exits[i].id == id) {
+          this.exits.splice(i, 1)
+          this.httpClient.delete(`${this.enxitsServices.url}/deleteExit/${id}`).subscribe(
+            res => {
+              console.log(res)
+            }, (error: any) => {
+              console.log(error);
+            }
+          )
+
+          return
+        }
       }
     }
+
+    alert("Operation canceled")
   }
 
-  add (){
-    if (this.exit.id == 0){
+  add() {
+
+    let id: any
+    id = this.route.snapshot.params
+
+    if (this.exit.id == 0) {
       alert("You must type a code diferent to 0")
       return
     }
-    for (let i = 0; i < this.exits.length; i++){
-      if (this.exits[i].id == this.exit.id){
+    for (let i = 0; i < this.exits.length; i++) {
+      if (this.exits[i].id == this.exit.id) {
         alert('The database contains a item with the same code')
         return
       }
     }
 
-    this.exits.push({id:this.exit.id, amount: this.exit.amount, concept: this.exit.concept})
+    this.exits.push({ id: this.exit.id, amount: this.exit.amount, concept: this.exit.concept, date: this.exit.date })
+
+    this.httpClient.post(this.enxitsServices.url + '/addExit', { id: this.exit.id, amount: this.exit.amount, concept: this.exit.concept, date: this.exit.date, personId: id.id })
+      .subscribe(
+        res => {
+          console.log(res)
+        }, (error: any) => {
+          console.log(error)
+        }
+      )
 
     this.exit.id = 0
     this.exit.amount = 0.0
@@ -52,28 +80,29 @@ export class ExitsComponent {
 
   }
 
-  select(exit: {id:number, amount:number, concept:string}){
+  select(exit: { id: number, amount: number, concept: string, date: string }) {
     this.exit.id = exit.id
     this.exit.amount = exit.amount
     this.exit.concept = exit.concept
-}
-
-modify(){
-  for (let i = 0 ; i < this.exits.length; i++){
-    if (this.exits[i].id == this.exit.id){
-      this.exits[i].amount = this.exit.amount
-      this.exits[i].concept = this.exit.concept
-      this.exits[i].date = this.exit.date
-
-      return
-    }
+    this.exit.date = exit.date
   }
-  alert ("the typed entry doesn't exist on database")
-}
 
-ngOnInit(){
-  let id:any
-  id = this.route.snapshot.params
-  this.exits = this.enxitsServices.getExits(id.id)
-}
+  modify() {
+    for (let i = 0; i < this.exits.length; i++) {
+      if (this.exits[i].id == this.exit.id) {
+        this.exits[i].amount = this.exit.amount
+        this.exits[i].concept = this.exit.concept
+        this.exits[i].date = this.exit.date
+
+        return
+      }
+    }
+    alert("the typed entry doesn't exist on database")
+  }
+
+  ngOnInit() {
+    let id: any
+    id = this.route.snapshot.params
+    this.exits = this.enxitsServices.getExits(id.id)
+  }
 }
