6fa6e80dd18d60cc4956e678b839bfc9e3928418
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-02-23 13:19:51 -0600

    Update: Internal Error component

--- Money-Coach/src/app/shared/internal-error/internal-error.component.ts
+++ Money-Coach/src/app/shared/internal-error/internal-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-internal-error',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class InternalErrorComponent {
 
+  constructor(private router: Router){}
+  
+  returnButton(){
+    this.router.navigate([''])
+  }
 }
