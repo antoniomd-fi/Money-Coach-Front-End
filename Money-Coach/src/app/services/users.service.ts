e392d5247419c753b07d6c849d0204b470ac61f0
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2023-03-01 12:04:35 -0600

    Update: Links

--- Money-Coach/src/app/services/users.service.ts
+++ Money-Coach/src/app/services/users.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class UsersService {
 
-  public url = 'hhttps://money-coach-db.herokuapp.com'
+  public url = 'http://localhost:3000'
 
   constructor( private httpClient:HttpClient, private router:Router) { }
 
